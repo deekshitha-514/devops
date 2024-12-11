7bbb2369fff8a8a9aec4c32a50eee2b784f202c8
deekshitha-514 <deekshithacse514@gmail.com>
2024-12-11 16:23:54 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -5,7 +5,7 @@ public class AddTwoNumbers {
             return;
         }
         int num1=Integer.parseInteger(args[0]);
-        int num2=Integer.parseInteger(args[1];
+        int num2=Integer.parseInteger(args[1]);
         int sum = num1+num2;
         System.out.println("The sum of "+ num1 +" and " + num2 + "is:"+sum);
        }
