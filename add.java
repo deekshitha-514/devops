44e32e1bd35978bc872f32c89ab1711a0a9aea68
deekshitha-514 <deekshithacse514@gmail.com>
2024-12-12 14:49:27 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class add{
+public static void main(String args[]){
+Scanner scanner=new scanner(System.in)
+system.out.println("Enter the first number: ");
+double num1 = scanner nextDouble();
+system.out.println("Enter the second number: ");
+double num2 = scanner nextDouble();
+double sum = num1 +num2;
+System.out.println("the sum of "+num1 + "and" +num2 + "is:"+sum);
+Scanner.close();
+}
+}
